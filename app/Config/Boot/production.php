aba281b0654433a7745c727a3173e70135b6d711
Yahya Zulfikri <zedlabs@hotmail.com>
2025-06-14 04:18:25 +0700

    Inisialisasi.

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -1,1 +1,4 @@
-<?php ini_set('display_errors', '0'); error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED); defined('CI_DEBUG') || define('CI_DEBUG', false);
+<?php
+ini_set('display_errors', '0');
+error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+defined('CI_DEBUG') || define('CI_DEBUG', false);
--- app/Config/Boot/testing.php
+++ app/Config/Boot/testing.php
@@ -1,1 +1,5 @@
-<?php error_reporting(-1); ini_set('display_errors', '1'); defined('SHOW_DEBUG_BACKTRACE') || define('SHOW_DEBUG_BACKTRACE', true); defined('CI_DEBUG') || define('CI_DEBUG', true);
+<?php
+error_reporting(-1);
+ini_set('display_errors', '1');
+defined('SHOW_DEBUG_BACKTRACE') || define('SHOW_DEBUG_BACKTRACE', true);
+defined('CI_DEBUG') || define('CI_DEBUG', true);
